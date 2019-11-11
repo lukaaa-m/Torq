691f6630464d8dbf6f1e8639904fcd42431d1493
lukaaa-m <lukablueowl51@gmail.com>
2019-11-12 08:42:38 +1000

    Brought together UI and function changes
    Client searches for hostname, connects to server then chat GUI opens. Needs functionality to open another server search after chat is closed.

--- client-class.py
+++ client-class.py
@@ -21,6 +21,7 @@ class Client:
         receive_thread = Thread(target=self.receive)
         receive_thread.start()
 
+<<<<<<< Updated upstream
         #self.sock.connect(('localhost',42069))
 
         tk.mainloop()
@@ -29,21 +30,49 @@ class Client:
         temp_msg = self.msg.get() #Gets msg from tk input field
         self.msg.set('') #Clears input field
         self.sock.send(temp_msg.encode())
+=======
+        tk.mainloop()
+
+        
+
+
+    def send(self, event=None):
+        temp_msg = self.msg.get() #Gets msg from tkinter input field
+        if(len(temp_msg) > 1014):
+            self.msg_list.configure(state="normal")
+            self.msg_list.insert(tk.END, 'Message exceeds 1014 characters!' + '\n')
+            self.msg_list.configure(state="disabled")
+            self.msg_list.see(tk.END)
+            self.msg.set('')
+        else:
+            self.msg.set('') #Clears input field
+            self.sock.send(temp_msg.encode())
+>>>>>>> Stashed changes
 
         if temp_msg == '{quit}': #Closes socket and chat window if window is exited
             self.sock.close()
-            self.chat_window.quit()
+            self.chat_window.destroy()
 
     def receive(self):
         while True:
             try:
                 new_message = self.sock.recv(self.bufsize).decode() #Receive message from server
+<<<<<<< Updated upstream
                 print(new_message)
                 if new_message == 'send hostname':
                     self.sock.send(bytes(os.getlogin(), 'utf8'))
                 else:
                     self.msg_list.insert('end', new_message) #Add new msg to chat history 
             
+=======
+                if new_message == 'send hostname':
+                    self.sock.send(bytes(os.getlogin(), 'utf8'))
+                else:
+                    self.msg_list.configure(state="normal")
+                    self.msg_list.insert(tk.END, new_message + '\n') #Add new msg to chat history
+                    self.msg_list.configure(state="disabled")
+                    self.msg_list.see(tk.END)
+>>>>>>> Stashed changes
             except OSError: #Other client may have left the chat
                 #print('No socket yet')
                 continue    
@@ -53,26 +82,34 @@ class Client:
         self.chat_window.title('Torq')
 
         #Frame for chat window
+<<<<<<< Updated upstream
         self.chat_frame = tk.Frame(self.chat_window)
+=======
+        self.chat_frame = tk.Frame(self.chat_window, bg="#36393e")
+>>>>>>> Stashed changes
         self.msg = tk.StringVar()  #For the messages to be sent
         self.msg.set("")
         self.scrollbar = tk.Scrollbar(self.chat_frame)  #To navigate through past messages
 
         #Box to contain message history
+<<<<<<< Updated upstream
         self.msg_list = tk.Listbox(self.chat_frame, height=15, width=50, yscrollcommand=self.scrollbar.set)
+=======
+        self.msg_list = tk.Text(self.chat_frame, height=25, width=60, yscrollcommand=self.scrollbar.set, wrap=tk.WORD, padx=10, font=("Verdana", 10), bg="#36393e", fg="white", spacing1=6, selectborderwidth=0, bd=0, selectbackground="gray")
+        self.msg_list.configure(state="disabled")
+>>>>>>> Stashed changes
         self.scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
         self.msg_list.pack(side=tk.LEFT, fill=tk.BOTH)
-        self.msg_list.pack()
         self.chat_frame.pack()
 
         #Message input field
-        self.msg_field = tk.Entry(self.chat_window, textvariable=self.msg)
+        self.msg_field = tk.Entry(self.chat_window, textvariable=self.msg, width=64, justify='left', font=("Verdana", 10), bg='#484B52', fg='white', selectborderwidth=0, bd=0, selectbackground="gray")
         self.msg_field.bind("<Return>", self.send)
-        self.msg_field.pack()
-        self.send_button = tk.Button(self.chat_window, text="Send", command=self.send)
-        self.send_button.pack()
+        self.msg_field.pack(side="top", fill="x")
 
         self.chat_window.protocol("WM_DELETE_WINDOW", self.onClosing)
+        self.chat_window.resizable(False, False)
+        
 
     def onClosing(self, event=None):
         self.msg.set('{quit}')

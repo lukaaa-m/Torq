c6578c6b4f986f46c85410362c548d9ac1fc4b7c
lukaaa-m <43409006+lukaaa-m@users.noreply.github.com>
2020-12-02 23:52:38 +1000

    Updated list

--- badwords.py
+++ badwords.py
@@ -1,15 +1,7 @@
 bad_words = [
-    'fuck',
-    'nigga',
-    'cunt',
-    'shit',
     'yes',
     'the',
-    'dick',
-    'pussy',
-    'nigger',
     'hello',
-    'cum',
     'english',
     'nose',
     'potato'
